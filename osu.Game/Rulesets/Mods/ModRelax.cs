3f19f50f0a7197d424babcd2c0bce9321ab367ae
Max Persson <admin@cloudmodding.com>
2020-03-06 08:38:34 +0100

    Made Relaxed ranked.

--- osu.Game/Rulesets/Mods/ModRelax.cs
+++ osu.Game/Rulesets/Mods/ModRelax.cs
@@ -12,6 +12,7 @@ namespace osu.Game.Rulesets.Mods
         public override string Name => "Relax";
         public override string Acronym => "RX";
         public override IconUsage? Icon => OsuIcon.ModRelax;
+        public override bool Ranked => true;
         public override ModType Type => ModType.Automation;
         public override double ScoreMultiplier => 1;
         public override Type[] IncompatibleMods => new[] { typeof(ModAutoplay), typeof(ModNoFail), typeof(ModSuddenDeath) };
